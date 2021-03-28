f0be7e8512fe738819eff79e2ee212e49c4599a9
Young-ook Kim <youngook.kim@gmail.com>
2021-03-29 00:45:08 +0900

    enable CORS

--- src/products/src/products-service/handlers.go
+++ src/products/src/products-service/handlers.go
@@ -161,4 +161,6 @@ func ProductFeatured(w http.ResponseWriter, r *http.Request) {
 // enableCors
 func enableCors(w *http.ResponseWriter) {
 	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Authorization")
 }
